575fb4ce60a0defe3dd4776c85b2553b8feb68f7
eduardofilho10 <62343184+eduardofilho10@users.noreply.github.com>
2021-05-04 11:26:26 -0300

    update base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/api/';
+$config['base_url'] = 'https://eduardofilho10.github.io/codeigniter_api_p_reactjs/';
 
 /*
 |--------------------------------------------------------------------------
